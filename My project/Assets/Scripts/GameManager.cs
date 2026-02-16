bc0caab66366222d8bfa97bee02a3e02ce511ad7
nayttyan <152870652+nayttyan@users.noreply.github.com>
2026-02-16 14:49:19 +0200

    abitOfCode

--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -1,18 +1,62 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject[] zombies;
     public GameObject selectedZombie;
+    public Vector3 selectedSize;
+    public Vector3 pushForce;
+    private InputAction next, prev, jump;
+    private int selectedIndex = 0;
 
     void Start()
     {
-        selectedZombie = zombies[2];
+        next = InputSystem.actions.FindAction("NextZombie");
+        prev = InputSystem.actions.FindAction("PrevZombie");
+        jump = InputSystem.actions.FindAction("Jump");
+        SelectZombie(selectedIndex);
+    }
+
+    void SelectZombie(int index)
+    {
+        if(selectedZombie != null)
+        {
+            selectedZombie.transform.localScale = Vector3.one;
+        }
+        
+        selectedZombie = zombies[index];
+        selectedZombie.transform.localScale = selectedSize;
+        Debug.Log("selected: " + selectedZombie);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(next.WasPressedThisFrame())
+        {
+            Debug.Log("next");
+            selectedIndex++;
+            if(selectedIndex >= zombies.Length) { selectedIndex = 0; }
+            SelectZombie(selectedIndex);
+        }
+        if (prev.WasPressedThisFrame())
+        {
+            Debug.Log("prev");
+            selectedIndex--;
+            if(selectedIndex < 0)
+                { selectedIndex = 3; }
+            SelectZombie(selectedIndex);
+        }
+
+        if(jump.WasPressedThisFrame())
+        {
+            Debug.Log("jump");
+            Rigidbody rb = selectedZombie.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                rb.AddForce(pushForce);
+            }
+        }
     }
 }
