9aae33def3f590dc157793500b1ceaa71cb16b80
nayttyan <152870652+nayttyan@users.noreply.github.com>
2026-02-22 19:37:56 +0200

    Fixees

--- My project/Assets/Scripts/CollectablesManager.cs
+++ My project/Assets/Scripts/CollectablesManager.cs
@@ -10,20 +10,17 @@ public class CollectablesManager : MonoBehaviour
 
     void Start()
     {
-        // Собираем все монетки-дети
         for (int i = 0; i < transform.childCount; i++)
         {
             GameObject coin = transform.GetChild(i).gameObject;
             coins.Add(coin);
         }
 
-        // Выключаем все монеты
         for (int i = 0; i < coins.Count; i++)
         {
             coins[i].SetActive(false);
         }
 
-        // Включаем первые 2 монеты
         for (int i = 0; i < coinsAtSameTime; i++)
         {
             SpawnRandomCoin();
@@ -32,19 +29,16 @@ public class CollectablesManager : MonoBehaviour
 
     void SpawnRandomCoin()
     {
-        // Ищем все монеты, которые сейчас выключены (и ещё не собраны окончательно)
         List<GameObject> available = new List<GameObject>();
 
         for (int i = 0; i < coins.Count; i++)
         {
-            // Если монета выключена, значит её можно включить
             if (coins[i] != null && coins[i].activeSelf == false)
             {
                 available.Add(coins[i]);
             }
         }
 
-        // Если нет доступных - нечего спавнить
         if (available.Count == 0) return;
 
         int randomIndex = Random.Range(0, available.Count);
@@ -53,18 +47,15 @@ public class CollectablesManager : MonoBehaviour
 
     public void CoinCollected(GameObject coin)
     {
-        // Монета "подобрана" -> выключаем её
         coin.SetActive(false);
 
         collectedCount++;
 
-        // Если собрали все 15 - больше ничего не спавним
         if (collectedCount >= coins.Count)
         {
             return;
         }
 
-        // Поддерживаем 2 монеты на сцене
         SpawnRandomCoin();
     }
 }
